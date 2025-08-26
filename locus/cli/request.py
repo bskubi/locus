850fe1d4cb49716fc51f2a686f95bca81d0735bc
Ben Skubi <bskubi@gmail.com>
2025-08-26 15:09:01 -0700

    Error on fail to load from json

--- locus/cli/request.py
+++ locus/cli/request.py
@@ -9,8 +9,13 @@ from locus.requests.hictk_request import hictk_request
 @click.argument("requests_jsons", nargs=-1)
 def request(requests_jsons):
     for request_json in requests_jsons:
-        request = json.loads(request_json)
-        assert request.get("type"), "Must specify 'type' key from options: [hictk]" 
+        try:
+            request = json.loads(request_json)
+            assert request.get("type"), "Must specify 'type' key from options: [hictk]" 
+        except Exception as e:
+            error = request.copy()
+            error["error"] = str(e)
+            print(error)
         if request.get("type") == "hictk":
             keys = hictk_request(request)
             for key in keys:
